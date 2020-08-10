ec2d2fcbe9d10b8f070a8db6f13d3e74ed271a9f
Thomas van den Bulk <Thomasvdbulk@gmail.com>
2020-08-10 23:48:37 +0200

    Cleaned up front page. Using flexbox

--- index.php
+++ index.php
@@ -1,53 +1,33 @@
-<html>
 
+<?php
+    $filename = "./controls/data.txt";
+    $file = fopen($filename, "r") or die("Unable to open file!");
+    $data= fread($file,filesize($filename));
+    fclose($file);
+    $data=explode(";",$data);
+
+    $tijd  = $data[0]; //zet sqlresult om in var
+    $start = $data[1]; //zet sqlresult om in var
+?>
+<html>
 <head>
     <title>Teller</title>
     <link href="./style.css" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 	<meta http-equiv="refresh" content="60" >
 </head>
-
 <body>
-<br>
-<a class="btn-lg btn-warning" href='./controls/index.php' >Invoer</a>
+    <a class="position-fixed btn-lg btn-warning settings" href='./controls/index.php'>Invoer</a>
 
-<?php
-$filename = "./controls/data.txt";
-$file = fopen($filename, "r") or die("Unable to open file!");
-$data= fread($file,filesize($filename));
-fclose($file);
-$data=explode(";",$data);
-
-	$tijd  = $data[0]; //zet sqlresult om in var
-	$start = $data[1]; //zet sqlresult om in var		
-
-
-?>
-
-        
-    
-    <div class="container-fluid">
-        <div class="row center leden">
-            <div class="col-md-4"></div>
-            <div class="col-md-4 Aantal">Er zijn
+    <div class="container h-100 w-100">
+        <div class="row h-100 w-100 align-items-center justify-content-center text-center">
+            <div class="col aantal">Er zijn
                 <br><span id="number"><?php echo htmlspecialchars($start); ?></span>
                 <br>nieuwe Catenianen bij!
                 <p id="update">Laatste geupdate: <?php echo htmlspecialchars($tijd); ?></p>
             </div>
-
-            <div class="col-md-4"></div>
         </div>
-
     </div>
 </body>
-
 </html>
